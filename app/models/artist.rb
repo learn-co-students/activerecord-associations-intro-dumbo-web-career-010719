2360c054ab32738c437360d1c455fc4540333fdb
Phog Allen <fieldhouse@Playbook.local>
2019-01-20 19:11:03 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,12 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+    has_many :genres, through: :songs
 end
+
+=begin
+    Because the songs table has an artist_id column
+    and because the Artist class uses the has_many macro,
+    an artist has many songs!
+
+    It is also true that an artist has many genres through songs
+=end
